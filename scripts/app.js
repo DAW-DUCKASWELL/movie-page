7fc79e3451563c419c96169ee41a9e05d2214501
DAW-DUCKASWELL <114958225+DAW-DUCKASWELL@users.noreply.github.com>
2023-04-02 06:54:49 -0400

    Funcionalidad para la sección de películas populares.
    Se agregó funcionalidad al apartado de películas populares y ahora las consume de TMDB.

---
+++ scripts/app.js
@@ -0,0 +1,57 @@
+`use strict`
+//paginación
+let page = 1;
+const btnPrevious = document.querySelector('.popular-movies__btnChangePage-previous')
+const btnNext = document.querySelector('.popular-movies__btnChangePage-next')
+
+btnNext.addEventListener('click', ()=>{
+    if(page < 1000){
+        page = page + 1;
+        loadMovies()
+    }
+})
+
+btnPrevious.addEventListener('click', ()=>{
+    if(page > 1){
+        page = page - 1;
+        loadMovies()
+    }
+})
+
+//MOSTRAR PELICULAS POPULARES
+//esperamos que la respuesta llegue para continuar.
+//*funciones async usamos try/catch
+//códigos de tmdb: 200, 401, 404.
+const loadMovies = async () => {
+    try{
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=45b49c30d379e8a24e7a536d9c44d2d9&language=es-VE&page=${page}`)
+        if(response.status === 200){ //métemos if para verificar que encontramos la pelicula.
+            const data = await response.json(); //-> método asincrono.
+            let movies = ''; //acá estaran todas las peliculas.
+            data.results.forEach(movie => {
+                let release_date = movie.release_date 
+                let year = release_date.split('-')//-> para que solo nos muestre el año.
+                movies = movies + `<div class="popular-movies__div data">
+                <a href="#" class="popular-movies__link-poster"><img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Aquí está el poster de una de las peliculas mejor valoradas." class="popular-movies__img"></a> 
+                <h3 class="popular-movies__title"><a href="">${movie.title}</a></h3>
+                <p class="popular-movies__year">${year[0]}</p>
+                </div>`
+            });
+            document.querySelector('.popular-movies').innerHTML = movies; //Código dentor del html
+
+        }else if(response.status === 401){
+            console.log('No tienes acceso a la base de datos. Llave incorrecta.')
+        }else if(response.status ===404){
+            console.log('Lo siento, la película que buscas no existe.')
+        }else{
+            console.log('Unknow error!')
+        }
+        
+         
+    }catch(error){
+        console.log(error);
+    }
+    //catch muestra error de código NO EN LA PETICIÓN EN SI.
+}
+
+loadMovies();
