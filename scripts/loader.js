d4c2105aaf217697bf31489ce981a8d9075ef134
DAW-DUCKASWELL <114958225+DAW-DUCKASWELL@users.noreply.github.com>
2023-04-03 22:55:50 -0400

    Algunos arreglos.

--- scripts/loader.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-    const loader = document.querySelector('.lds-ring')
-    const loader2 = document.querySelector('.lds-ring--large')
-    loader.style.display = "none"
-    loader2.style.display = "none"
-    this.console.log('entré')
-})
