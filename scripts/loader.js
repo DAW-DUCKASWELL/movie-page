717520b54cff7a46a8b3a201b1cad6051da2cf4c
DAW-DUCKASWELL <114958225+DAW-DUCKASWELL@users.noreply.github.com>
2023-04-03 22:02:10 -0400

    Agregado loaders a la web.
    Se agregaron loaders en las partes donde se consume la base de datos.

---
+++ scripts/loader.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    const loader = document.querySelector('.lds-ring')
+    const loader2 = document.querySelector('.lds-ring--large')
+    loader.style.display = "none"
+    loader2.style.display = "none"
+    this.console.log('entré')
+})
